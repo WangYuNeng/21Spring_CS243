1349b071309d34e61c904a714b54d64ab9d2b559
Yu-Neng Wang <wynwyn@myth60.stanford.edu>
2022-04-16 15:58:55 -0700

    Finish MySolver

--- hw2/src/submit/MySolver.java
+++ hw2/src/submit/MySolver.java
@@ -2,6 +2,9 @@ package submit;
 
 // some useful things to import. add any additional imports you need.
 import joeq.Compiler.Quad.*;
+
+import java.util.Iterator;
+
 import flow.Flow;
 
 /**
@@ -36,7 +39,104 @@ public class MySolver implements Flow.Solver {
         /***********************
          * Your code goes here *
          ***********************/
+        Boolean hasUpdate = true;
 
+        if (analysis.isForward()) {
+            while (hasUpdate == true) {
+                QuadIterator qit = new QuadIterator(cfg);
+                hasUpdate = false;
+                while (qit.hasNext()) {
+                    Quad q = qit.next();
+                    Flow.DataflowObject inOld = analysis.newTempVar(), meetObj = analysis.newTempVar();
+                    meetObj.setToTop();
+                    inOld.copy(analysis.getIn(q));
+                    Iterator<Quad> predQit = qit.predecessors();
+
+                    while (predQit.hasNext()) {
+                        Quad predQ = predQit.next();
+                        if (predQ == null) { // predecessor is entry
+                            meetObj.meetWith(analysis.getEntry());
+                        }
+                        else {
+                            meetObj.meetWith(analysis.getOut(predQ));
+                        }
+                    }
+                    analysis.setIn(q, meetObj);
+                    analysis.processQuad(q);
+
+                    if (!analysis.getIn(q).equals(inOld)) {
+                        hasUpdate = true;
+                    }
+                }
+            }
+
+            // Handle exit node
+            Flow.DataflowObject meetObj = analysis.newTempVar();
+            meetObj.setToTop();
+            QuadIterator qit = new QuadIterator(cfg);
+            while (qit.hasNext()) {
+                Quad q = qit.next();
+                
+                Iterator<Quad> succQit = qit.successors();
+
+                while (succQit.hasNext()) {
+                    Quad succQ = succQit.next();
+                    if (succQ == null) { // successor is exit
+                        meetObj.meetWith(analysis.getOut(q));
+                    }
+                }
+                analysis.setExit(meetObj);
+            }
+        }
+
+        else {
+            while (hasUpdate == true) {
+                QuadIterator qit = new QuadIterator(cfg, false);
+                hasUpdate = false;
+                while (qit.hasPrevious()) {
+                    Quad q = qit.previous();
+                    Flow.DataflowObject outOld = analysis.newTempVar(), meetObj = analysis.newTempVar();
+                    meetObj.setToTop();
+                    outOld.copy(analysis.getOut(q));
+                    Iterator<Quad> succQit = qit.successors();
+
+                    while (succQit.hasNext()) {
+                        Quad succQ = succQit.next();
+                        if (succQ == null) { // successor is exit
+                            meetObj.meetWith(analysis.getExit());
+                        }
+                        else {
+                            meetObj.meetWith(analysis.getIn(succQ));
+                        }
+                    }
+                    analysis.setOut(q, meetObj);
+                    analysis.processQuad(q);
+
+                    if (!analysis.getOut(q).equals(outOld)) {
+                        hasUpdate = true;
+                    }
+                }
+            }
+
+            // Handle exit node
+            Flow.DataflowObject meetObj = analysis.newTempVar();
+            meetObj.setToTop();
+            QuadIterator qit = new QuadIterator(cfg, false);
+            while (qit.hasPrevious()) {
+                Quad q = qit.previous();
+                
+                Iterator<Quad> predQit = qit.predecessors();
+
+                while (predQit.hasNext()) {
+                    Quad predQ = predQit.next();
+                    if (predQ == null) { // predecessor is entry
+                        meetObj.meetWith(analysis.getIn(q));
+                    }
+                }
+                analysis.setEntry(meetObj);
+            }
+        }
+        
         // this needs to come last.
         analysis.postprocess(cfg);
     }
